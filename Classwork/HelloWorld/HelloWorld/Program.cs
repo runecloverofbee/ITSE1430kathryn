11050e8404dc9fff69634730a3dcbe28d502d696
runecloverofbee <kathryn.bessent@my.tccd.edu>
2018-08-22 19:15:31 -0500

    End of Second class 8/22/2018

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,17 @@ namespace HelloWorld
     {
         static void Main( string[] args )
         {
+            //string name;
+            //name = "Bob";
+            string name = "Bob";
+            //string Name;
+
+            double payRate = 4.65;
+            Console.WriteLine(name);
+
+            /*double y = 4.65;
             int x = 10;
+            */
             Console.WriteLine("Hello world");
         }
     }
