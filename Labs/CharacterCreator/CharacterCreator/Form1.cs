7df5c9a3de7e14d7be1f7b2ac10e7ea149d350ca
kathryn.bessent <kathryn.bessent@my.tccd.edu>
2018-09-28 12:04:05 -0500

    number 2 done

---
+++ Labs/CharacterCreator/CharacterCreator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterCreator
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit?",
+                 "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Close();
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Kathryn Bessent\nIntorduction to C#\nCharacter Creator","About", MessageBoxButtons.OK);
+        }
+    }
+}
